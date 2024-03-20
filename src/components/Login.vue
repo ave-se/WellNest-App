b18a13bb5c89916ce5cf2700c4a5d3a873c09490
Agnes-Cybil Atsuah <agnescybil.atsuah@hyperisland.se>
2024-03-20 09:56:39 +0100

    New Login form file

---
+++ src/components/Login.vue
@@ -0,0 +1,40 @@
+<!-- src/components/Login.vue -->
+
+<template>
+    <div class="login">
+      <form @submit.prevent="loginUser">
+        <label for="email">Email:</label>
+        <input id="email" v-model="credentials.email" type="email">
+  
+        <label for="password">Password:</label>
+        <input id="password" v-model="credentials.password" type="password">
+  
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import { mapActions } from 'vuex'
+  
+  export default {
+    data() {
+      return {
+        credentials: {
+          email: '',
+          password: ''
+        }
+      }
+    },
+    methods: {
+      ...mapActions('user', ['loginUser']),
+      loginUser() {
+        this.loginUser(this.credentials)
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
